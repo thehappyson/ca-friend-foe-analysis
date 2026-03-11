81b06a2bda2b50cbb8c96dd6c79551bed8370a56
Kunkel, Kevin <kevin.kunkel@exxeta.com>
2026-03-11 14:34:39 +0100

    remove redundant/outdated classes

--- filelist.py
+++
@@ -1,8 +0,0 @@
-import os
-import csv
-
-f= open('./data/frames/annotation.csv','r+')
-w=csv.writer(f)
-for files in os.walk('./data/frames/jud_suess'):
-    for filename in files:
-        w.writerow([filename])
